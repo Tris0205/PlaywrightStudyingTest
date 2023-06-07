4e0e7c7ed6a0118495c90ca9aa9e808ad1190e72
cr4yontran <131322912+cr4yontran@users.noreply.github.com>
2023-06-07 15:28:55 +0700

    Update generateCustomLayout.ts

--- generateCustomLayout.ts
+++ generateCustomLayout.ts
@@ -1,43 +1,15 @@
-// import { Block, KnownBlock } from '@slack/types';
-// import { SummaryResults } from "playwright-slack-report/dist/src";;
-// import { slackReporter, getReportUrl } from 'playwright-slack-report';
-// import TOKEN from './token';
-
-// export default function generateCustomLayoutSimpleExample(
-//   summaryResults: SummaryResults,
-// ): Array<Block | KnownBlock> {
-//   const slackOAuthToken = 'xoxb-5245659500131-5258569071793-SIOBLWsAXOA62zamKqKHqZNR';
-//   const reporter = slackReporter({ token: slackOAuthToken });
-//   const reportUrl = getReportUrl(reporter);
-
-//    return [
-//     {
-//       type: 'section',
-//       text: {
-//         type: 'mrkdwn',
-//         text:
-//           summaryResults.failed < 1
-//             ? ':tada: All tests passed!'
-//             : `😭${summaryResults.failed} failure(s) out of ${summaryResults.tests.length} tests`,
-//       },
-//     },
-//     {
-//       type: 'section',
-//       text: {
-//         type: 'mrkdwn',
-//         text: `Allure Report: ${reportUrl}`,
-//       },
-//     },
-//   ];
-// }
-
 import { Block, KnownBlock } from '@slack/types';
 import { SummaryResults } from "playwright-slack-report/dist/src";;
+import { slackReporter, getReportUrl } from 'playwright-slack-report';
 
 export default function generateCustomLayoutSimpleExample(
   summaryResults: SummaryResults,
 ): Array<Block | KnownBlock> {
-  return [
+  const slackOAuthToken = 'xoxb-5245659500131-5258569071793-SIOBLWsAXOA62zamKqKHqZNR';
+  const reporter = slackReporter({ token: slackOAuthToken });
+  const reportUrl = getReportUrl(reporter);
+
+   return [
     {
       type: 'section',
       text: {
@@ -48,5 +20,32 @@ export default function generateCustomLayoutSimpleExample(
             : `😭${summaryResults.failed} failure(s) out of ${summaryResults.tests.length} tests`,
       },
     },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `Allure Report: ${reportUrl}`,
+      },
+    },
   ];
 }
+
+// import { Block, KnownBlock } from '@slack/types';
+// import { SummaryResults } from "playwright-slack-report/dist/src";;
+
+// export default function generateCustomLayoutSimpleExample(
+//   summaryResults: SummaryResults,
+// ): Array<Block | KnownBlock> {
+//   return [
+//     {
+//       type: 'section',
+//       text: {
+//         type: 'mrkdwn',
+//         text:
+//           summaryResults.failed < 1
+//             ? ':tada: All tests passed!'
+//             : `😭${summaryResults.failed} failure(s) out of ${summaryResults.tests.length} tests`,
+//       },
+//     },
+//   ];
+// }
