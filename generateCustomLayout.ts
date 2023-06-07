9574a4ab923a5a1572fb91971d9a20365045d9a1
cr4yontran <131322912+cr4yontran@users.noreply.github.com>
2023-06-07 14:10:56 +0700

    Update generateCustomLayout.ts

--- generateCustomLayout.ts
+++ generateCustomLayout.ts
@@ -1,10 +1,16 @@
 import { Block, KnownBlock } from '@slack/types';
 import { SummaryResults } from "playwright-slack-report/dist/src";;
+import { slackReporter, getReportUrl } from 'playwright-slack-report';
+import TOKEN from './token';
 
 export default function generateCustomLayoutSimpleExample(
   summaryResults: SummaryResults,
 ): Array<Block | KnownBlock> {
-  return [
+  const slackOAuthToken = 'xoxb-5245659500131-5258569071793-SIOBLWsAXOA62zamKqKHqZNR';
+  const reporter = slackReporter({ token: slackOAuthToken });
+  const reportUrl = getReportUrl(reporter);
+
+   return [
     {
       type: 'section',
       text: {
@@ -13,6 +19,13 @@ export default function generateCustomLayoutSimpleExample(
           summaryResults.failed < 1
             ? ':tada: All tests passed!'
             : `😭${summaryResults.failed} failure(s) out of ${summaryResults.tests.length} tests`,
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `Allure Report: ${reportUrl}`,
       },
     },
   ];
