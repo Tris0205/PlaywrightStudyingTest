c6b34c86fb0736d47424ef6db255dceff60bba88
cr4yontran <131322912+cr4yontran@users.noreply.github.com>
2023-06-07 15:13:08 +0700

    Update generateCustomLayout.ts

--- generateCustomLayout.ts
+++ generateCustomLayout.ts
@@ -1,16 +1,43 @@
+// import { Block, KnownBlock } from '@slack/types';
+// import { SummaryResults } from "playwright-slack-report/dist/src";;
+// import { slackReporter, getReportUrl } from 'playwright-slack-report';
+// import TOKEN from './token';
+
+// export default function generateCustomLayoutSimpleExample(
+//   summaryResults: SummaryResults,
+// ): Array<Block | KnownBlock> {
+//   const slackOAuthToken = 'xoxb-5245659500131-5258569071793-SIOBLWsAXOA62zamKqKHqZNR';
+//   const reporter = slackReporter({ token: slackOAuthToken });
+//   const reportUrl = getReportUrl(reporter);
+
+//    return [
+//     {
+//       type: 'section',
+//       text: {
+//         type: 'mrkdwn',
+//         text:
+//           summaryResults.failed < 1
+//             ? ':tada: All tests passed!'
+//             : `😭${summaryResults.failed} failure(s) out of ${summaryResults.tests.length} tests`,
+//       },
+//     },
+//     {
+//       type: 'section',
+//       text: {
+//         type: 'mrkdwn',
+//         text: `Allure Report: ${reportUrl}`,
+//       },
+//     },
+//   ];
+// }
+
 import { Block, KnownBlock } from '@slack/types';
 import { SummaryResults } from "playwright-slack-report/dist/src";;
-import { slackReporter, getReportUrl } from 'playwright-slack-report';
-import TOKEN from './token';
 
 export default function generateCustomLayoutSimpleExample(
   summaryResults: SummaryResults,
 ): Array<Block | KnownBlock> {
-  const slackOAuthToken = 'xoxb-5245659500131-5258569071793-SIOBLWsAXOA62zamKqKHqZNR';
-  const reporter = slackReporter({ token: slackOAuthToken });
-  const reportUrl = getReportUrl(reporter);
-
-   return [
+  return [
     {
       type: 'section',
       text: {
@@ -19,13 +46,6 @@ export default function generateCustomLayoutSimpleExample(
           summaryResults.failed < 1
             ? ':tada: All tests passed!'
             : `😭${summaryResults.failed} failure(s) out of ${summaryResults.tests.length} tests`,
-      },
-    },
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: `Allure Report: ${reportUrl}`,
       },
     },
   ];
